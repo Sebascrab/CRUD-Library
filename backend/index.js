34301286e34766049d2393588ff23f7014dedbc8
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-03-05 22:26:58 -0700

    started db

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mysql from 'mysql';
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: "localhost", 
+    user: "root", 
+    password: "3577", 
+    database: "test"
+});
+
+app.get("/", (req, res) => {
+    res.json("Hello, this is the backend!");
+});
+
+app.listen(8800, () => {
+    console.log('Connected to backend!');
+});
